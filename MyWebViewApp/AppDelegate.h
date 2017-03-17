d17b5781c122376ca6360caece71192124174597
daxapps <daxapps777@gmail.com>
2017-03-17 15:43:43 -0500

    Initial Commit

---
+++ MyWebViewApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWebViewApp
+//
+//  Created by Jason Crawford on 3/17/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
